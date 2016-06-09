d1385f305a27b09355f521dd793b67a827ea867f
AroraParmeet <parmeet@geeksperhour.com>
2016-06-09 18:54:40 +0530

    show more and less text

--- custom.js
+++ custom.js
@@ -1,1 +1,17 @@
-alert("hello world");
+<div class="category-header brandDesc descCollapsed" id="categoryHeader">
+    <script type="text/javascript">
+      function showMore() {
+        jQuery('.brandDesc').toggleClass('descCollapsed').toggleClass('descOpened');
+        jQuery('#showMore').toggleClass('hidden');
+        jQuery('#showLess').toggleClass('hidden');
+      }
+      function showLess() {
+        jQuery('.brandDesc').toggleClass('descOpened').toggleClass('descCollapsed');
+        jQuery('#showLess').toggleClass('hidden');
+        jQuery('#showMore').toggleClass('hidden');
+      } 
+    </script>
+     <img src="assets/images/01_adrenal.jpg" alt="" border="0px"><br>  <div style="font-size: 12pt;">Adrenal Support products aid in the relief of both stress and fatigue, as well as the promotion of a properly functioning adrenal system. They have been shown to support the adrenal cortex and to boost the immune system. Roughly eight out of ten people suffer from a condition known as “adrenal fatigue”, the symptoms of which are: unexplained tiredness, morning grogginess, a desire for caffeine, and a general stressed and run down feeling. The Adrenal Support supplements found at Covenant Health Products help to bring your adrenal system back into order.</div>       
+      <a href="javascript:void(0);" class="showToggle hidden" style="margin-top: -1.1em; font-weight: normal;" id="showLess" onclick="showLess();">           Show less...</a>
+      <a href="javascript:void(0);" class="showToggle" style="top: 7.5em; font-weight: normal;" id="showMore" onclick="showMore();">Show more...</a>
+    </div>
