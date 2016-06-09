00365a1568866081e50ee3ced88240460e65669c
AroraParmeet <parmeet@geeksperhour.com>
2016-06-09 19:01:48 +0530

    show more and less jquery

--- custom.js
+++ custom.js
@@ -11,7 +11,9 @@
         jQuery('#showMore').toggleClass('hidden');
       } 
     </script>
+    <!--- html code 
      <img src="assets/images/01_adrenal.jpg" alt="" border="0px"><br>  <div style="font-size: 12pt;">Adrenal Support products aid in the relief of both stress and fatigue, as well as the promotion of a properly functioning adrenal system. They have been shown to support the adrenal cortex and to boost the immune system. Roughly eight out of ten people suffer from a condition known as “adrenal fatigue”, the symptoms of which are: unexplained tiredness, morning grogginess, a desire for caffeine, and a general stressed and run down feeling. The Adrenal Support supplements found at Covenant Health Products help to bring your adrenal system back into order.</div>       
       <a href="javascript:void(0);" class="showToggle hidden" style="margin-top: -1.1em; font-weight: normal;" id="showLess" onclick="showLess();">           Show less...</a>
       <a href="javascript:void(0);" class="showToggle" style="top: 7.5em; font-weight: normal;" id="showMore" onclick="showMore();">Show more...</a>
     </div>
+    !-->
